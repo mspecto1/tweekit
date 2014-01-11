6dab1bdb92ae56bdb6bd85f16c50f401bd18e530
Marley Spector <marley.spector@gmail.com>
2014-01-10 20:01:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweekit::Application.config.secret_key_base = '0e6e0a9f3f4d4ea1f821fc3b27f91a41ef2b03a774f9e10f3383622074467dd430a35f5f3222ecba676e550c44f2d1f6442b2589fca5c0c515f17be01f4259dd'
