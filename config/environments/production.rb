2816e739792a9b1aa7826e1c56215b12fc6b768a
Marley Spector <marley.spector@gmail.com>
2014-01-13 19:41:39 -0500

    setup devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Tweekit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://tweekit.herokuapp.com/' }
 end
